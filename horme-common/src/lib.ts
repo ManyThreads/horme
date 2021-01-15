afe8d32628c19327f88c06f70d2952337bf44b5d
Bernhard Sebastian Brüggemann <bruegber@b-tu.de>
2021-01-15 11:59:46 +0100

    common: hide runtypes types and added create methods for common types
    Signed-off-by: Bernhard Sebastian Brüggemann <bruegber@b-tu.de>

--- horme-common/src/lib.ts
+++ horme-common/src/lib.ts
@@ -1,30 +1,33 @@
-import { Array, Literal, Null, Number, String, Record, Union } from "runtypes";
+import { Array, Literal, Null, Number, Static, String, Record, Runtype, Union } from "runtypes";
 
 export { default as env, MqttAuth, Environment, ServiceEnvironment } from './env';
 export { default as util } from './util';
 
 /** The transmitted values of binary sensors. */
-export const Value = Union(Literal('on'), Literal('off'));
+const ValueR = Union(Literal("on"), Literal("off"));
+export type Value = Static<typeof ValueR>
 
 /** The format for device messages. */
-export const DeviceMessage = Record({
+const DeviceMessageR = Record({
     apartment: String,
     location: String,
     uuid: String,
     type: String,
-    value: Value,
+    value: ValueR,
     sensor: String.Or(Null),
     timestamp: Number,
 });
+export type DeviceMessage = Static<typeof DeviceMessageR>;
 
 /** The format for a service failure message. */
-export const FailureMessage = Record({
+const FailureMessageR = Record({
     uuid: String,
     reason: String,
 });
+export type FailureMessage = Static<typeof FailureMessageR>;
 
 /** The Service information sent with each configuration message. */
-export const ServiceInfo = Record({
+export const ServiceInfoR = Record({
     topic: String,
     apartment: String,
     location: String,
@@ -32,17 +35,46 @@ export const ServiceInfo = Record({
     type: String,
     sensor: String.Or(Null)
 });
+export type ServiceInfo = Static<typeof ServiceInfoR>;
 
 /** The contents of a transmitted service subscription announcement. */
-export const Subscription = Record({
+const SubscriptionR = Record({
     uuid: String,
     type: String,
     topic: String,
 });
+export type Subscription = Static<typeof SubscriptionR>;
 
 /** The format for configuration messages. */
-export const ConfigMessage = Record({
-    info: ServiceInfo,
-    add: Array(Subscription),
-    del: Array(Subscription),
+export const ConfigMessageR = Record({
+    info: ServiceInfoR,
+    add: Array(SubscriptionR),
+    del: Array(SubscriptionR),
 });
+export type ConfigMessage = Static<typeof ConfigMessageR>;
+
+/** The configuration for starting a service instance of a type. */
+const ServiceConfigR = Record({
+    sensor: String.Or(Null),
+    cmd: Record({
+        exec: String,
+        args: Array(String)
+    })
+});
+
+export type ServiceConfig = Static<typeof ServiceConfigR>
+
+const checkT = <T, R extends Runtype<T>>(msg: any, r: R): T | undefined => {
+    try {
+        return r.check(msg);
+    } catch (error) {
+        return undefined;
+    }
+}
+
+export const createDeviceMessage = (msg: any) => checkT<DeviceMessage, typeof DeviceMessageR>(msg, DeviceMessageR);
+export const createFailureMessage = (msg: any) => checkT<FailureMessage, typeof FailureMessageR>(msg, FailureMessageR);
+export const createServiceInfo = (msg: any) => checkT<ServiceInfo, typeof ServiceInfoR>(msg, ServiceInfoR);
+export const createSubscription = (msg: any) => checkT<Subscription, typeof SubscriptionR>(msg, SubscriptionR);
+export const createConfigMessage = (msg: any) => checkT<ConfigMessage, typeof ConfigMessageR>(msg, ConfigMessageR);
+export const createServiceConfig = (msg: any) => checkT<ServiceConfig, typeof ServiceConfigR>(msg, ServiceConfigR);
