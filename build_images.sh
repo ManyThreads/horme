c28e5c83963b5ba1c14ee6ba9811eb310f545c7e
Oliver Giersch <oliver.giersch@b-tu.de>
2021-01-29 10:45:02 +0100

    adjusts container build script to fail early

--- build_images.sh
+++ build_images.sh
@@ -1,5 +1,5 @@
-docker build -t ceiling-lamp -f ./services/ceiling-lamp/Dockerfile .
-docker build -t light-switch -f ./services/light-switch/Dockerfile .
+docker build -t ceiling-lamp -f ./services/ceiling-lamp/Dockerfile . && \
+docker build -t light-switch -f ./services/light-switch/Dockerfile . && \
 docker build -t reconf -f ./reconf/Dockerfile .
 #docker build -t failure-reasoner -f services/failure-reasoner/Dockerfile .
 #cd services/camera-motion-detect
